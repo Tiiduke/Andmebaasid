66a8313b9025d1e1f4cf5e9c6e36afe5a6d508ef
Tiiduke <tiithendrik.piibeleht@gmail.com>
2016-04-03 21:29:27 +0300

    Started work, got documentation done on cards, added all variables

--- src/main/java/Card.java
+++ src/main/java/Card.java
@@ -2,4 +2,14 @@
  * Created by Tiit on 2.04.2016.
  */
 public class Card {
+
+    private String name;
+    private int cardSet;
+    private boolean collectible;
+    private int rarity;
+    private int cardType;
+    private int cost;
+    private int classType;
+    private String textOnCard;
+    private String flavorText;
 }
--- src/main/java/Cardreader.java
+++ src/main/java/Cardreader.java
@@ -1,7 +1,22 @@
+import org.w3c.dom.NodeList;
+import sun.plugin.dom.core.Document;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.File;
+
 /**
  * Created by Tiit on 2.04.2016.
  */
 public class Cardreader {
 
+    public static void main(String[] args) throws Exception{
+        File input = new File("CardDefs.xml");
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        org.w3c.dom.Document document = builder.parse(input);
 
+        NodeList cardList = document.getElementsByTagName("Entity");
+
+    }
 }
