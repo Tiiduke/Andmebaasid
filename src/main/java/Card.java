bfd0a8213b6ff13567438bf41f93f54a2d49b5b2
Tiiduke <tiithendrik.piibeleht@gmail.com>
2016-04-04 22:17:47 +0300

    Property finding work, next step is assignment to card

--- src/main/java/Card.java
+++ src/main/java/Card.java
@@ -12,4 +12,76 @@ public class Card {
     private int classType;
     private String textOnCard;
     private String flavorText;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCardSet() {
+        return cardSet;
+    }
+
+    public void setCardSet(int cardSet) {
+        this.cardSet = cardSet;
+    }
+
+    public boolean isCollectible() {
+        return collectible;
+    }
+
+    public void setCollectible(boolean collectible) {
+        this.collectible = collectible;
+    }
+
+    public int getRarity() {
+        return rarity;
+    }
+
+    public void setRarity(int rarity) {
+        this.rarity = rarity;
+    }
+
+    public int getCardType() {
+        return cardType;
+    }
+
+    public void setCardType(int cardType) {
+        this.cardType = cardType;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+
+    public int getClassType() {
+        return classType;
+    }
+
+    public void setClassType(int classType) {
+        this.classType = classType;
+    }
+
+    public String getTextOnCard() {
+        return textOnCard;
+    }
+
+    public void setTextOnCard(String textOnCard) {
+        this.textOnCard = textOnCard;
+    }
+
+    public String getFlavorText() {
+        return flavorText;
+    }
+
+    public void setFlavorText(String flavorText) {
+        this.flavorText = flavorText;
+    }
 }
--- src/main/java/Cardreader.java
+++ src/main/java/Cardreader.java
@@ -1,3 +1,5 @@
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import sun.plugin.dom.core.Document;
 
@@ -11,6 +13,17 @@ import java.io.File;
 //Test
 public class Cardreader {
 
+
+
+    public static void assignProperty(Card card, Element element) {
+        switch(element.getAttribute("name")) {
+            case "CardName": NodeList languages = element.getElementsByTagName("enUS");
+                System.out.println(languages.item(0).getTextContent()); break;
+            default: break;
+
+        }
+    }
+
     public static void main(String[] args) throws Exception{
         File input = new File("CardDefs.xml");
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
@@ -19,5 +32,23 @@ public class Cardreader {
 
         NodeList cardList = document.getElementsByTagName("Entity");
 
+        for (int i = 0; i < cardList.getLength(); i++) {
+            Node node = cardList.item(i);
+
+            if (node.getNodeType() == Node.ELEMENT_NODE) {
+                Card card = new Card();
+
+                Element element = (Element) node;
+
+                NodeList properties = element.getElementsByTagName("Tag");
+
+                for (int j = 0; j < properties.getLength(); j++) {
+                    Element property = (Element) properties.item(j);
+                    assignProperty(card, property);
+                }
+
+            }
+
+        }
     }
 }
